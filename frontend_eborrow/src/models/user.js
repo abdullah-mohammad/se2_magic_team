0164e1574b5aad01b317c0529cff26d1cf6314a2
Matthias Mildenberger <matthias.mildenberg@xing.com>
2020-12-02 11:53:00 +0100

    userjs wieder hergestellt wtf

--- frontend_eborrow/src/models/user.js
+++ frontend_eborrow/src/models/user.js
@@ -1,12 +1,12 @@
 export default class User {
   constructor(username, password, firstname, lastname, email, gender, profilepicture, birthdate) {
-    this.username = username;
-    this.password = password;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
     this.gender = gender;
-    this.profilepicture = profilepicture;
-    this.birthdate = birthdate; 
+        this.profilepicture = profilepicture;
+        this.birthdate = birthdate;
   }
-}
+    }
