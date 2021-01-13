68be31dcad8f6c57b0f525b2f745cbafabb623bc
acq573 <Aleksandra.Dymchishina@haw-hamburg.de>
2021-01-13 14:39:35 +0100

    controller test corrected

--- src/test/java/de/haw/eborrow/controller/ControllerTests.java
+++ src/test/java/de/haw/eborrow/controller/ControllerTests.java
@@ -2,6 +2,7 @@
 package de.haw.eborrow.controller;
 
 import com.jayway.jsonpath.JsonPath;
+import de.haw.eborrow.models.Address;
 import de.haw.eborrow.models.Item;
 import de.haw.eborrow.models.User;
 import de.haw.eborrow.repository.UserRepository;
@@ -59,7 +60,9 @@ class ItemControllerTest {
 	@Test
 	void getAllItems() throws Exception {
 		RequestBuilder request = MockMvcRequestBuilders.get("/items");
-		User user = new User("user", "$2a$10$MeWhrWoE2yvclNulZyCZb.IDxlJIhQDMbm8pOVhH3DRGFaz7ZpaaS", "User 1", "isLastaName", "user@using.com", "w", new Date(111), "pic.jpg");
+		User user = new User("user", "$2a$10$MeWhrWoE2yvclNulZyCZb.IDxlJIhQDMbm8pOVhH3DRGFaz7ZpaaS", "User 1", "isLastaName", "user@using.com", "w", new Date(111),
+				"pic.jpg", new Address("Berliner Tor", "1", 20099, "Hamburg", "Germany"));
+
 		Item item = new Item("Rasenmaeher", "Gutes Geraet, ich liebe es, liebst du es auch?", "maeh.jpg", true, user);
 
 		MvcResult result = mvc.perform(request)
