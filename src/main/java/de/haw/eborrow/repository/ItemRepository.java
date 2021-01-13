a4ce5292ae3a67bb2134423b0a39f4b616eb06fd
Venance Konan <venancekonan5@gmail.com>
2021-01-13 13:48:55 +0100

    set

--- src/main/java/de/haw/eborrow/repository/ItemRepository.java
+++ src/main/java/de/haw/eborrow/repository/ItemRepository.java
@@ -1,16 +1,30 @@
 package de.haw.eborrow.repository;
 
+import de.haw.eborrow.models.Borrow;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import de.haw.eborrow.models.Item;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
+import java.util.Collection;
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
+
     List<Item> findByAvailable(boolean available);
 
     List<Item> findByTitleContaining(String title);
+
+    /*SELECT * FROM ITEM i
+    WHERE i.ID NOT IN (
+            SELECT b.ITEM_ID   FROM BORROW b
+            WHERE '2021-01-28' <= b. BORROW_TO  AND '2021-01-30' >= BORROW_FROM
+    )*/
 
     @Query("SELECT i" +
             " FROM Item i" +
